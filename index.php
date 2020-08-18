7c7aeea0898f40e46c0c577a996019e7c3ee823f
Apasih-coding <anuapayah1@gmail.com>
2020-08-18 11:55:29 +0700

    insert file php

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+ 	<meta name="author" content="width=device-width, initial-scale=1">
+	<title>Portofolio Teguh</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	 <link rel="stylesheet" href="font/font-awsome/css/font-awesome.min.css">
+	 <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="container-fluid" id="menu">
+		<nav id="navbar-scroll" class="navbar navbar-expand-lg navbar-light bg-light-blue">
+		  <a class="navbar-brand" href="#Home">
+		  	<img src="img/brand.png" id="navbar-brand">
+		  </a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav mr-auto">
+		      <li class="nav-item">
+		        <a class="nav-link" href="#Home">Home</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#About">About</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#Work">Work</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#Contact">Contact</a>
+		      </li>
+		      <li class="nav-item call">
+		        <a class="nav-link disabled " href="#Contact">call me <span class="num">081288450678</span></a>
+		      </li>	      
+		      
+		      <li class="animation"></li>	
+		         
+			</ul>  
+		  </div>		  
+		</nav>
+		
+	</div>
+
+	<div data-spy="scroll" data-target="#navbar-scroll" data-offset="0">
+		<section id="Home">			
+			<div class="jumbotron">								
+				<div class="media">
+					<div class="media-body text-center">
+						<img src="img/img.png" alt="John Doe" class="ml-3 mt-3 pull-right">
+						<p class="nama">Hello<a class="num" href="#">.</a> i am Teguh</p>						
+				 	</div>
+				</div>
+
+				<div class="dev">
+					<p>Back End Developer</p>					
+				</div>
+				<div class="webdev">
+					<p>Web Developer</p>
+				</div>					
+			</div>
+		</section>
+
+		<section class="container-fluid" id="About">
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="card card-about text-center">
+						<p>About Me</p>
+					</div>
+					<div class="card card-tentang">
+				      <div class="card-body">
+				        <p class="card-text">Saya sarjana komputer dari UNIVERSITAS JENDERAL ACHMAD YANI Yogyakarta program studi Teknik Informatika, dan  merupakan seseorang yang sangat tertarik dengan dunia sistem informasi berbasis web, saya sangat percaya segala sesuatu dapat dipelajari dan dikuasai.
+				        Saat ini saya menjaga dan mengasah kemampuan web programing saya dengan membuat beberapa sistem web. Saya memiliki kesehatan yang sangat baik. Dengan sifat disiplin, ramah, pekerja keras walau pemalu. Saya yakin dapat berkontribusi dalam pekerjaan ini.</p>
+				      </div>
+				    </div>
+
+				    <div class="card card-skill">
+						<p class="skill">Skill</p>
+						<div class="bar-skill">
+							<div class="row col-sm-12">
+								<div class="col-sm-3">
+									<p>PhP</p>
+								</div>
+								<div class="col-sm-9">
+									<div class="progress">
+										<div class="progress-bar bg-secondary" style="width:80%"></div>
+									</div>
+								</div>
+							</div>
+							<div class="row col-sm-12">
+								<div class="col-sm-3">
+									<p>MySQL</p>
+								</div>
+								<div class="col-sm-9">
+									<div class="progress">
+										<div class="progress-bar bg-secondary" style="width:85%"></div>
+									</div>
+								</div>
+							</div>
+							<div class="row col-sm-12">
+								<div class="col-sm-3">
+									<p>Javascript</p>
+								</div>
+								<div class="col-sm-9">
+									<div class="progress">
+										<div class="progress-bar bg-secondary" style="width:55%"></div>
+									</div>
+								</div>
+							</div>
+							<div class="row col-sm-12">
+								<div class="col-sm-3">
+									<p>HTML</p>
+								</div>
+								<div class="col-sm-9">
+									<div class="progress">
+										<div class="progress-bar bg-secondary" style="width:80%"></div>
+									</div>
+								</div>
+							</div>
+							<div class="row col-sm-12">
+								<div class="col-sm-3">
+									<p>CSS</p>
+								</div>
+								<div class="col-sm-9">
+									<div class="progress">
+										<div class="progress-bar bg-secondary" style="width:75%"></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="pull-right">
+						<img src="img/Mask.png" class="img-responsive img-mask">
+					</div>
+					<div class="card card-img pull-right">
+						<img src="img/img2.png" class="img-responsive">
+					</div>
+				</div>
+			</div>			
+		</section>	
+
+		<section id="Work">
+			<div class="container-fluid">
+				<p class="work1">Work.</p>
+				<div class="hr1"></div>			
+				<div class="col-sm-12">
+					<div class="row">
+					<div class="col mb-4">
+						<div class="card text-center">						
+							<div class="card-project">
+								<img src="img/winih.png" class="img-top img-fluid">
+							</div>
+							<div class="content-project">
+								<p class="h3">Winih Q<br><span>Winih Q adalah toko online yang menyediakan berbagai macam benih, pupuk dan peralatan bertani ataupun berkebun. Winih Q mempunyai ambisi atau tujuan untuk memakmurkan petani di Indonesia.</span></p>
+
+							</div>
+						</div>
+					</div>
+					<div class="col mb-4">
+						<div class="card text-center">						
+							<div class="card-project">
+								<img src="img/ggg.png" class="img-top img-fluid">
+							</div>	
+							<div class="content-project">
+								<p class="h3">Winih Q<br><span>Winih Q adalah toko online yang menyediakan berbagai macam benih, pupuk dan peralatan bertani ataupun berkebun. Winih Q mempunyai ambisi atau tujuan untuk memakmurkan petani di Indonesia.</span></p>
+
+							</div>					
+						</div>
+					</div>
+					<div class="col mb-4">
+						<div class="card justify-content-center">
+							<div class="card-project">
+								<img src="" class="img-top img-fluid">
+							</div>	
+							<div class="content-project text-center">
+								<p class="h3">Next Project<br><span></span></p>
+
+							</div>
+						</div>
+					</div>
+				</div>
+				</div>			
+			</div>	
+		</section>
+
+		<section class="container-fluid" id="Contact">
+			<div class="row">
+				<div class="col-md-3">
+					<div class="card logo justify-content-center">
+						<img src="img/T.png" class="img-responsive T">
+					</div>
+				</div>
+				<div class="col-md-8">
+					<div class="card">
+						<div class="card-footer">
+							<p class="kontak">Contact</p>
+							<div class="card hr2"></div>
+						</div>
+						<form method="post" enctype="multipart/form-data">
+							<div class="row">
+								<div class="col">
+									<input type="email" class="form-control" id="inputEmail4" name="email" placeholder="Email" required>
+								</div>
+								<div class="col">
+									<input type="text" class="form-control" id="inputPassword4" name="subject" placeholder="Subject" required>
+								</div>
+							</div>  						    	 
+						    <textarea class="form-control" id="pesan" name="pesan" rows="3" placeholder="Pesan" required></textarea>			 
+							<button type="submit" class="btn btn-primary" id="submit" name="submit">Send</button>
+						</form>
+					</div>
+				</div>
+				<div class="col-md-1">
+					<div class="sosmed pull-right">			
+						<img src="img/ig.png" type="button" class="img-responsive ig" data-container="body" data-toggle="popover" data-placement="left" data-content="@teguh_dwi_saputra">
+
+						<img src="img/fb.png" type="button" class="img-responsive fb" data-container="body" data-toggle="popover" data-placement="left" data-content="teguh dwi saputra">
+
+						<img src="img/Rectangle47.png" class="img-responsive grs">
+					</div>
+				</div>
+			</div>
+			
+			
+		</section>	
+			
+					
+		
+
+	</div>
+
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="copy text-center">
+						<p>&copy; 2020 | Bulit by. <a href="http://instagram.com/teguh_dwi_saputra">Teguh Dwi Saputra</a> | Design by.<a href="http://instagram.com/lutffi"> Lutfi Windu</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+	<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="js/jquery-3.4.1.slim.js"></script>
+  	<script type="text/javascript" src="js/jquery-3.4.1.slim.min.js"></script>
+  	<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+  	<script>
+		$(document).ready(function(){
+		  $('[data-toggle="popover"]').popover();   
+		});
+	</script>
+</body>
+</html>
